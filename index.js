3a3b12aa35df895bb6d6cd8c38da0d4a9fc794cc
ciripel <nedelcu.alexandru@yahoo.com>
2021-04-26 19:29:36 +0300

    fixed bitbox dependency and rebuilt

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 "use strict";
 /// <reference path="./lib/vendors.d.ts"/>
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./lib/slp"));
-__export(require("./lib/utils"));
-__export(require("./lib/primatives"));
-__export(require("./lib/bitdbnetwork"));
-__export(require("./lib/localvalidator"));
-__export(require("./lib/bitboxnetwork"));
-__export(require("./lib/transactionhelpers"));
+exports.SlpAddressUtxoResult = exports.SlpUtxoJudgement = exports.SlpVersionType = exports.SlpTransactionType = exports.bitcore = void 0;
+__exportStar(require("./lib/slp"), exports);
+__exportStar(require("./lib/utils"), exports);
+__exportStar(require("./lib/primatives"), exports);
+__exportStar(require("./lib/bitdbnetwork"), exports);
+__exportStar(require("./lib/localvalidator"), exports);
+__exportStar(require("./lib/bitboxnetwork"), exports);
+__exportStar(require("./lib/transactionhelpers"), exports);
 var bitcore = __importStar(require("bitcore-lib-cash"));
 exports.bitcore = bitcore;
 var SlpTransactionType;
